a951dd059d037bd853be69df47b0fd96c5db07ac
unknown <edd.edwardmike@gmail.com>
2021-09-03 16:15:39 +0100

    newsletter can be saved

--- mailer/subscribe/views.py
+++ mailer/subscribe/views.py
@@ -14,6 +14,7 @@ def subscribe_for_newsletter(request,template='subscribe/index.html',*args,**kwa
 	if request.method == "POST":
 		form = forms.SubscribeForm(data = request.POST)
 		if form.is_valid():
+			form.save()
 			email = request.POST.get('email',None)
 
 			# celery email 
